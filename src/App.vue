5dbfdd75eceb3eb7dcb7ff4b70f1ca6618fed3f9
jrfs22 <mr.jrfs22@gmail.com>
2023-05-10 07:33:02 +0700

    PERTEMUAN 1 VUEJS CLI

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>IDShop</h1>
+    <p class="animate_animated animate_fadeInRight">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+export default{
+  name: 'App',
+  components: {
+    FontAwesomeIcon
+  },
+}
+</script>
